07986f444d1d67c27b3741dd42d97e5ded6d080b
davismurithi628-cell <davismurithi628@gmail.com>
2026-04-03 23:01:02 +0300

    Delete fastapicode.py

--- fastapicode.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from apirouters import usersloginapi,userssignupapi
-app = FastAPI()
-@app.get("/")
-def greet():
-    return "Hi Dave"
-app.include_router(userssignupapi.router)
